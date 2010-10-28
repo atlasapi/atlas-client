416095bc0bd13a5867846f370a36fc8c3391dfa4
Ben Smith <ben.thesmith@gmail.com>
2010-10-28 15:17:47 +0100

    added EnumAttributeBuilder

--- src/main/java/org/atlasapi/client/query/AnyQuery.java
+++ src/main/java/org/atlasapi/client/query/AnyQuery.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.atlasapi.content.criteria.AtomicQuery;
 import org.atlasapi.content.criteria.attribute.Attribute;
 import org.atlasapi.content.criteria.attribute.Attributes;
+import org.atlasapi.media.entity.Publisher;
 import org.atlasapi.media.entity.simple.ContentQueryResult;
 import org.atlasapi.media.entity.simple.Description;
 
@@ -15,6 +16,14 @@ public class AnyQuery extends AtlasQuery<Description> {
 
     AnyQuery(ImmutableList<AtomicQuery> conjuncts, Selection selection) {
         super(conjuncts, selection);
+    }
+    
+    public StringAttributeBuilder<Description> genre() {
+        return new StringAttributeBuilder<Description>(this, Attributes.DESCRIPTION_GENRE);
+    }
+    
+    public EnumAttributeBuilder<Description, Publisher> publisher() {
+        return new EnumAttributeBuilder<Description, Publisher>(this, Attributes.DESCRIPTION_PUBLISHER);
     }
 
     @Override
---
+++ src/main/java/org/atlasapi/client/query/EnumAttributeBuilder.java
@@ -0,0 +1,21 @@
+package org.atlasapi.client.query;
+
+import org.atlasapi.content.criteria.attribute.Attribute;
+import org.atlasapi.content.criteria.operator.Operators;
+
+import com.google.common.collect.ImmutableList;
+
+public class EnumAttributeBuilder<T, E extends Enum<E>> {
+
+    private final AtlasQuery<T> chain;
+    private final Attribute<Enum<E>> attribute;
+
+    public EnumAttributeBuilder(AtlasQuery<T> chain, Attribute<Enum<E>> attribute) {
+        this.chain = chain;
+        this.attribute = attribute;
+    }
+    
+    public AtlasQuery<T> equalTo(Enum<E>... values) {
+        return chain.add(attribute.createQuery(Operators.EQUALS, ImmutableList.copyOf(values)));
+    }
+}
