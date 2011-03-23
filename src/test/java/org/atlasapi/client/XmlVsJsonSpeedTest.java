ade47101f6091fe0659da87290184d9b67057f8d
Ben Smith <ben@thesmith.co.uk>
2011-03-23 10:23:07 +0000

    speed test for xml vs json

---
+++ src/test/java/org/atlasapi/client/XmlVsJsonSpeedTest.java
@@ -0,0 +1,40 @@
+package org.atlasapi.client;
+
+import static org.junit.Assert.assertFalse;
+
+import org.atlasapi.media.entity.simple.ContentQueryResult;
+import org.junit.Test;
+
+
+public class XmlVsJsonSpeedTest {
+    private final String urlBase = "http://otter.atlasapi.org/3.0/content.%s?uri=http://www.bbc.co.uk/programmes/b00vsvv5";
+    private final JsonQueryClient jsonClient = new JsonQueryClient();
+    private final JaxbStringQueryClient xmlClient = new JaxbStringQueryClient();
+    
+    @Test
+    public void jsonTest() {
+        String url = String.format(urlBase, "json");
+        ContentQueryResult query = jsonClient.query(url);
+        assertFalse(query.getContents().isEmpty());
+        
+        speedTest(jsonClient, url);
+    }
+    
+    @Test
+    public void xmlTest() {
+        String url = String.format(urlBase, "xml");
+        ContentQueryResult query = xmlClient.query(url);
+        assertFalse(query.getContents().isEmpty());
+        
+        speedTest(xmlClient, url);
+    }
+    
+    private void speedTest(StringQueryClient client, String url) {
+        long currentTimeMillis = System.currentTimeMillis();
+        for (int i=0; i<5; i++) {
+            client.query(url);
+        }
+        long took = System.currentTimeMillis() - currentTimeMillis;
+        System.out.println("Test took "+took+" millis");
+    }
+}
