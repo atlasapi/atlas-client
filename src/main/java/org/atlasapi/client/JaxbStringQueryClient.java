d8e184418d2886f791b1f34121566239e66832e3
John Ayres <john@metabroadcast.com>
2011-06-20 10:26:10 +0100

    avoid creating a big String

--- src/main/java/org/atlasapi/client/JaxbStringQueryClient.java
+++ src/main/java/org/atlasapi/client/JaxbStringQueryClient.java
@@ -1,61 +1,68 @@
 package org.atlasapi.client;
 
-import java.io.StringReader;
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
 
+import org.apache.http.HttpResponse;
 import org.atlasapi.media.entity.simple.ContentQueryResult;
 import org.atlasapi.media.entity.simple.PeopleQueryResult;
 import org.atlasapi.media.entity.simple.ScheduleQueryResult;
 
-import com.metabroadcast.common.http.HttpStatusCodeException;
+import com.metabroadcast.common.http.HttpException;
+import com.metabroadcast.common.http.HttpResponseTransformer;
 import com.metabroadcast.common.http.SimpleHttpClient;
 import com.metabroadcast.common.http.SimpleHttpClientBuilder;
 
 class JaxbStringQueryClient implements StringQueryClient {
 
-	private static final String USER_AGENT = "Mozilla/5.0 (compatible; atlas-java-client/1.0; +http://atlasapi.org)";
+    private static final String USER_AGENT = "Mozilla/5.0 (compatible; atlas-java-client/1.0; +http://atlasapi.org)";
 
-	private static final int NOT_FOUND = 404;
+    private final JAXBContext context;
+    private final SimpleHttpClient httpClient;
+    
+    
+    public JaxbStringQueryClient() {
+        try {
+            context = JAXBContext.newInstance(ContentQueryResult.class, ScheduleQueryResult.class, PeopleQueryResult.class);
+        } catch (JAXBException e) {
+            throw new RuntimeException(e);
+        }
+        httpClient = new SimpleHttpClientBuilder().withUserAgent(USER_AGENT).withSocketTimeout(1, TimeUnit.MINUTES).withTransformer(new JaxbTransformer()).build();
+    }
+    
+    private class JaxbTransformer implements HttpResponseTransformer<Object> {
+        @Override
+        public Object transform(HttpResponse response) throws HttpException, IOException {
+            try {
+                Unmarshaller unmarshaller = context.createUnmarshaller();
+                return unmarshaller.unmarshal(response.getEntity().getContent());
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+    
+    public Object queryInternal(String queryUri) {
+        try {
+            return httpClient.get(queryUri).transform();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-	private final SimpleHttpClient httpClient = new SimpleHttpClientBuilder().withUserAgent(USER_AGENT).withSocketTimeout(1, TimeUnit.MINUTES).build();
-	
-	private final JAXBContext context;
-	
-	public JaxbStringQueryClient() {
-		try {
-			context = JAXBContext.newInstance(ContentQueryResult.class, ScheduleQueryResult.class, PeopleQueryResult.class);
-		} catch (JAXBException e) {
-			throw new RuntimeException(e);
-		}
-	}
-	
-	public Object queryInternal(String queryUri) {
-		try {
-			Unmarshaller unmarshaller = context.createUnmarshaller();
-			return unmarshaller.unmarshal(new StringReader(httpClient.getContentsOf(queryUri)));
-		}  catch (HttpStatusCodeException e) {
-			if (NOT_FOUND == e.getStatusCode()) {
-				return new ContentQueryResult();
-			}
-			throw new RuntimeException(e);
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
-	}
+    @Override
+    public ScheduleQueryResult scheduleQuery(String queryUri) {
+        return (ScheduleQueryResult) queryInternal(queryUri);
+    }
 
-	@Override
-	public ScheduleQueryResult scheduleQuery(String queryUri) {
-		return (ScheduleQueryResult) queryInternal(queryUri);
-	}
-
-	@Override
-	public ContentQueryResult contentQuery(String queryUri) {
-		return (ContentQueryResult) queryInternal(queryUri);
-	}
+    @Override
+    public ContentQueryResult contentQuery(String queryUri) {
+        return (ContentQueryResult) queryInternal(queryUri);
+    }
 
     @Override
     public PeopleQueryResult peopleQuery(String queryUri) {
