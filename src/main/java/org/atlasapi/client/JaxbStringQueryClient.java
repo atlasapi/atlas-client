6bd5a98260307c050f82745007a26a6187c8a334
John Ayres <john@metabroadcast.com>
2011-07-06 12:30:25 +0100

    update to use new http client

--- src/main/java/org/atlasapi/client/JaxbStringQueryClient.java
+++ src/main/java/org/atlasapi/client/JaxbStringQueryClient.java
@@ -1,23 +1,27 @@
 package org.atlasapi.client;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.concurrent.TimeUnit;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.util.EntityUtils;
 import org.atlasapi.media.entity.simple.ContentQueryResult;
 import org.atlasapi.media.entity.simple.PeopleQueryResult;
 import org.atlasapi.media.entity.simple.ScheduleQueryResult;
 
+import com.google.common.base.Charsets;
+import com.google.common.io.CharStreams;
 import com.metabroadcast.common.http.HttpException;
+import com.metabroadcast.common.http.HttpResponsePrologue;
 import com.metabroadcast.common.http.HttpResponseTransformer;
 import com.metabroadcast.common.http.HttpStatusCode;
 import com.metabroadcast.common.http.SimpleHttpClient;
 import com.metabroadcast.common.http.SimpleHttpClientBuilder;
+import com.metabroadcast.common.http.SimpleHttpRequest;
 
 class JaxbStringQueryClient implements StringQueryClient {
 
@@ -33,29 +37,29 @@ class JaxbStringQueryClient implements StringQueryClient {
         } catch (JAXBException e) {
             throw new RuntimeException(e);
         }
-        httpClient = new SimpleHttpClientBuilder().withUserAgent(USER_AGENT).withSocketTimeout(1, TimeUnit.MINUTES).withTransformer(new JaxbTransformer()).build();
+        httpClient = new SimpleHttpClientBuilder().withUserAgent(USER_AGENT).withSocketTimeout(1, TimeUnit.MINUTES).build();
     }
     
-    private class JaxbTransformer implements HttpResponseTransformer<Object> {
+    private HttpResponseTransformer<Object> JAXB_TRANSFORMER = new HttpResponseTransformer<Object>() {
         @Override
-        public Object transform(HttpResponse response) throws HttpException, IOException {
-            int statusCode = response.getStatusLine().getStatusCode();
+        public Object transform(HttpResponsePrologue response, InputStream in) throws HttpException, IOException {
+            int statusCode = response.statusCode();
             if (!HttpStatusCode.OK.is(statusCode)) {
-                String body = EntityUtils.toString(response.getEntity());
-                throw new HttpException(body, new com.metabroadcast.common.http.HttpResponse(body, statusCode));
+                String body = CharStreams.toString(new InputStreamReader(in, Charsets.UTF_8));
+                throw new HttpException(body, response);
             }
             try {
                 Unmarshaller unmarshaller = context.createUnmarshaller();
-                return unmarshaller.unmarshal(response.getEntity().getContent());
+                return unmarshaller.unmarshal(in);
             } catch (Exception e) {
                 throw new RuntimeException(e);
             }
         }
-    }
+    };
     
     public Object queryInternal(String queryUri) {
         try {
-            return httpClient.get(queryUri).transform();
+            return httpClient.get(new SimpleHttpRequest<Object>(queryUri, JAXB_TRANSFORMER));
         } catch (Exception e) {
             throw new RuntimeException("Could not load " + queryUri + " from atlas", e);
         }
