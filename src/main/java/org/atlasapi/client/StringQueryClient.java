7bf2e631f30cddd93bb87970ad26a18d355bc3f9
Oli Hall <oli@metabradcast.com>
2012-08-21 17:52:45 +0100

    Added AtlasChannelClient Added GsonChannelClient ScheduleQuery now takes channel aliases/ channelIds as Strings, original methods are deprecated

--- src/main/java/org/atlasapi/client/StringQueryClient.java
+++ src/main/java/org/atlasapi/client/StringQueryClient.java
@@ -1,5 +1,7 @@
 package org.atlasapi.client;
 
+import org.atlasapi.media.entity.simple.ChannelGroupQueryResult;
+import org.atlasapi.media.entity.simple.ChannelQueryResult;
 import org.atlasapi.media.entity.simple.ContentGroupQueryResult;
 import org.atlasapi.media.entity.simple.ContentQueryResult;
 import org.atlasapi.media.entity.simple.PeopleQueryResult;
@@ -17,5 +19,9 @@ interface StringQueryClient {
 	PeopleQueryResult peopleQuery(String queryUri);
 	
 	TopicQueryResult topicQuery(String queryUri);
+	
+	ChannelQueryResult channelQuery(String queryUri);
+	
+	ChannelGroupQueryResult channelGroupQuery(String queryUri);
 	
 }
