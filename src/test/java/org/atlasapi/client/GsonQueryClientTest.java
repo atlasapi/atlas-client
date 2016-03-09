3c0a66f00f147406a121c482c3e69aabbb2f3149
Lavanya Jeyaratnam <lavanya@metabroadcast.com>
2016-03-09 10:49:15 +0000

    Modified test again...

--- src/test/java/org/atlasapi/client/GsonQueryClientTest.java
+++ src/test/java/org/atlasapi/client/GsonQueryClientTest.java
@@ -12,14 +12,12 @@ import com.google.gson.JsonSerializationContext;
 public class GsonQueryClientTest {
 
     @Mock JsonSerializationContext context;
-//DateTime(int year, int monthOfYear, int dayOfMonth, int hourOfDay, int minuteOfHour)
 
     @Test
     public void serializeDateTime() {
-        String date = "2016-03-09T00:00:00.000Z";
         DateTime dateTime = new DateTime(2016, 3, 9, 00, 00);
         GsonQueryClient.JodaDateTimeSerializer serializer = new GsonQueryClient.JodaDateTimeSerializer();
         JsonElement element = serializer.serialize(dateTime, DateTime.class, context);
-        assertEquals(date, element.getAsString());
+        assertEquals("2016-03-09T00:00:00.000Z", element.getAsString());
     }
 }
