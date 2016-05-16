a6701dcb9c210cc86ff757a113e20f7f4229fdb0
Martins <martins.git@irbe.co.uk>
2016-05-16 19:26:48 +0100

    Adding methods that takes api key to GsonContentGroupClient so that GsonContentGroupClient can be reused.

--- src/main/java/org/atlasapi/client/AtlasContentGroupClient.java
+++ src/main/java/org/atlasapi/client/AtlasContentGroupClient.java
@@ -7,9 +7,16 @@ import com.google.common.base.Optional;
 
 
 public interface AtlasContentGroupClient {
-    ContentGroupQueryResult contentGroup(String id);    
-    
-    ContentGroupQueryResult contentGroups();    
-    
+
+    ContentGroupQueryResult contentGroup(String id);
+
+    ContentGroupQueryResult contentGroup(String id, Optional<String> apiKey);
+
+    ContentGroupQueryResult contentGroups();
+
+    ContentGroupQueryResult contentGroups(Optional<String> apiKey);
+
     ContentQueryResult contentFor(String contentGroupid, Optional<ContentQuery> contentQuery);
+
+    ContentQueryResult contentFor(String contentGroupid, Optional<ContentQuery> contentQuery, Optional<String> apiKey);
 }
--- src/main/java/org/atlasapi/client/GsonContentGroupClient.java
+++ src/main/java/org/atlasapi/client/GsonContentGroupClient.java
@@ -22,32 +22,69 @@ public class GsonContentGroupClient implements AtlasContentGroupClient {
         this.contentGroupContentQueryPattern = String.format("http://%s/3.0/content_groups/%%s/content.json", atlasHost);
         this.client = new GsonQueryClient();
     }
-    
+
+    public GsonContentGroupClient(HostSpecifier atlasHost, Optional<String> apiKey,
+            GsonQueryClient client) {
+        this.apiKey = apiKey;
+        this.singleContentGroupQueryPattern = String.format("http://%s/3.0/content_groups/%%s.json", atlasHost);
+        this.contentGroupsQuery = String.format("http://%s/3.0/content_groups.json", atlasHost);
+        this.contentGroupContentQueryPattern = String.format("http://%s/3.0/content_groups/%%s/content.json", atlasHost);
+        this.client = client;
+    }
+
     @Override
     public ContentGroupQueryResult contentGroup(String id) {
-        return client.contentGroupQuery(appendApiKey(String.format(singleContentGroupQueryPattern, id)));
+        return this.contentGroup(id, apiKey);
     }
-    
+
+    @Override
+    public ContentGroupQueryResult contentGroup(String id, Optional<String> apiKey) {
+        return client.contentGroupQuery(appendApiKey(
+                String.format(singleContentGroupQueryPattern, id),
+                apiKey
+        ));
+    }
+
     @Override
     public ContentGroupQueryResult contentGroups() {
-        return client.contentGroupQuery(appendApiKey(contentGroupsQuery));
+        return this.contentGroups(apiKey);
     }
-    
+
     @Override
-    public ContentQueryResult contentFor(String contentGroupid, Optional<ContentQuery> contentQuery) {
-        String queryString = appendApiKey(String.format(contentGroupContentQueryPattern, contentGroupid));
+    public ContentGroupQueryResult contentGroups(Optional<String> apiKey) {
+        return client.contentGroupQuery(appendApiKey(
+                contentGroupsQuery,
+                apiKey
+        ));
+    }
+
+    @Override
+    public ContentQueryResult contentFor(String contentGroupId, Optional<ContentQuery> contentQuery) {
+        return this.contentFor(contentGroupId, contentQuery, apiKey);
+    }
+
+    @Override
+    public ContentQueryResult contentFor(String contentGroupId, Optional<ContentQuery> contentQuery,
+            Optional<String> apiKey) {
+        String queryString = appendApiKey(
+                String.format(contentGroupContentQueryPattern, contentGroupId),
+                apiKey
+        );
+        return executeContentQuery(contentQuery, queryString);
+    }
+
+    private ContentQueryResult executeContentQuery(Optional<ContentQuery> contentQuery,
+            String queryString) {
         if (contentQuery.isPresent()) {
             queryString = Urls.appendParameters(queryString, contentQuery.get().toQueryStringParameters());
         }
         return client.contentQuery(queryString);
     }
-    
-    private String appendApiKey(String uri) {
+
+    private String appendApiKey(String uri, Optional<String> apiKey) {
         if (apiKey.isPresent()) {
             return Urls.appendParameters(uri, "apiKey", apiKey.get());
         }
         return uri;
     }
-   
-
 }
