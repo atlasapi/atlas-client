86ef0c73fa85707d17af4237ec589c93af29df7f
Jamie Coleman <jamiec@metabroadcast.com>
2017-02-02 11:49:03 +0000

    update test

--- src/test/java/org/atlasapi/client/JaxbAtlasClientTest.java
+++ src/test/java/org/atlasapi/client/JaxbAtlasClientTest.java
@@ -2,18 +2,10 @@ package org.atlasapi.client;
 
 import static org.atlasapi.output.Annotation.AVAILABLE_LOCATIONS;
 import static org.atlasapi.output.Annotation.UPCOMING;
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
-import static org.mockito.Mockito.mock;
 
-import com.metabroadcast.applications.client.model.internal.Application;
-import org.atlasapi.media.entity.simple.ContentGroup;
-import org.atlasapi.media.entity.simple.ContentGroupQueryResult;
 import org.atlasapi.media.entity.simple.ContentQueryResult;
-import org.atlasapi.media.entity.simple.PeopleQueryResult;
 import org.atlasapi.media.entity.simple.Playlist;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import com.google.common.collect.Iterables;
@@ -22,9 +14,15 @@ public class JaxbAtlasClientTest {
 
     @Test
     public void testContentContentQuery() {
-        ContentQuery contentQuery = ContentQuery.builder().withUrls("http://pressassociation.com/brands/8306").withAnnotations(UPCOMING, AVAILABLE_LOCATIONS).build();
+        ContentQuery contentQuery = ContentQuery.builder()
+                .withUrls("http://pressassociation.com/brands/8306")
+                .withAnnotations(UPCOMING, AVAILABLE_LOCATIONS)
+                .build();
         
-        AtlasClient client = new JaxbAtlasClient("http://stage.atlas.metabroadcast.com/3.0", "59be198386c143a7badb3b20a03ca042");
+        AtlasClient client = new JaxbAtlasClient(
+                "http://atlas-stage.metabroadcast.com/3.0",
+                "59be198386c143a7badb3b20a03ca042"
+        );
         
         ContentQueryResult content = client.content(contentQuery);
         Playlist result = (Playlist) Iterables.getOnlyElement(content.getContents());
